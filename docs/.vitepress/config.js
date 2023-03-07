f46dd609630d109a82e6f1f5f41b45853ad839aa
Merrick <2316546236@qq.com>
2023-03-07 16:16:26 +0800

    'upupup'

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -74,7 +74,9 @@ export default {
           items: [
             { text: 'C语言', link: '/notes/1' },
             { text: '科幻', link: '/notes/2' },
-            { text: '个人成长', link: '/notes/3' }
+            { text: '个人成长', link: '/notes/3' },
+            { text: '交易', link: '/notes/4' },
+            { text: '外语学习', link: '/notes/5' }
           ]
         }
       ]
